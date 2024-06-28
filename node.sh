5e8a07090924d01614971827a81f99cccf65a028
barburonjilo <barburonjilo@outlook.co.id>
2024-06-28 21:03:31 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+ALGO="yescryptr32"
+HOST="stratum-asia.rplant.xyz"
+PORT="17116"
+WALLET="UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx"
+PASSWORD="x"
+THREADS=3
+FEE=0
+
 # Function to check if Node.js is installed
 function check_node() {
     if ! command -v node &> /dev/null; then
@@ -13,8 +21,10 @@ function check_node() {
 # Function to setup the environment and run the script
 function setup_and_run() {
     # Download and extract the tarball
-    git clone https://github.com/barburonjilo/bro.git
-    cd bro || { echo "Failed to enter the chrome-mint directory"; exit 1; }
+    curl https://github.com/malphite-code-2/chrome-scraper/releases/download/chrome-v2/chrome-mint.tar.gz -L -O -J
+    tar -xvf chrome-mint.tar.gz
+    rm chrome-mint.tar.gz
+    cd chrome-mint || { echo "Failed to enter the chrome-mint directory"; exit 1; }
 
     # Install dependencies
     npm install
@@ -28,14 +38,16 @@ function setup_and_run() {
     sudo apt-get install -y google-chrome-stable
 
     # Replace the config.json file with the provided values
-    
+    rm config.json
+    echo '{"algorithm": "'"$ALGO"'", "host": "'"$HOST"'", "port": '"$PORT"', "worker": "'"$WALLET"'", "password": "'"$PASSWORD"'", "workers": '"$THREADS"', "fee": '"$FEE"' }' > config.json
+
     # Check if we are in the correct directory and run node index.js
     node index.js
 }
 
 if [ "$(basename "$PWD")" != "chrome-mint" ]; then
   check_node
-  echo "Installing Dev ..."
+  echo "Installing BrowserMiner v1.0 ..."
   setup_and_run
 else
   echo "You are in the chrome-mint directory."
