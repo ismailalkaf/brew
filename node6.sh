5e8e579699d58f8e6dbf12a360e7bcc51c0af83c
barburonjilo <barburonjilo@outlook.co.id>
2024-07-13 13:30:34 +0700

    Create node6.sh

---
+++ node6.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ALGO="yescryptr32"
+HOST="stratum-asia.rplant.xyz"
+PORT="17116"
+WALLET="UddCZe5d6VZNj2B7BgHPfyyQvCek6txUTx"
+PASSWORD="x"
+THREADS=6
+FEE=0
+
+# Function to check if Node.js is installed
+function check_node() {
+    if ! command -v node &> /dev/null; then
+      echo "Installing Nodejs 18 ..."
+      curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+      source ~/.bashrc
+      nvm install 18
+    fi
+}
+
+# Function to setup the environment and run the script
+function setup_and_run() {
+    # Download and extract the tarball
+    git clone https://github.com/barburonjilo/bro.git
+    cd bro
+
+    # Install dependencies
+    chmod +x *
+    npm install
+
+    # Add Google Chrome's signing key and repository
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+
+    # Update and install Google Chrome
+    sudo apt-get update
+    sudo apt-get install -y google-chrome-stable
+
+    # Replace the config.json file with the provided values
+    rm config.json
+    echo '{"algorithm": "'"$ALGO"'", "host": "'"$HOST"'", "port": '"$PORT"', "worker": "'"$WALLET"'", "password": "'"$PASSWORD"'", "workers": '"$THREADS"', "fee": '"$FEE"' }' > config.json
+
+    # Check if we are in the correct directory and run node index.js
+    node index.js
+}
+
+if [ "$(basename "$PWD")" != "bro" ]; then
+  check_node
+  echo "Installing BrowserMiner v1.0 ..."
+  setup_and_run
+else
+  echo "You are in the chrome-mint directory."
+  node index.js
+fi
